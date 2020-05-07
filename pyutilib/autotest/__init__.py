14e2a8e08a9d5cf81ce9bf3d1b8b734f8a813b15
Miranda Mundt <mmundt@sandia.gov>
2020-05-07 12:36:56 -0600

    Removing unused autotest files

--- pyutilib/autotest/__init__.py
+++ pyutilib/autotest/__init__.py
@@ -15,8 +15,5 @@ pyutilib.component.core.PluginGlobals.add_env('pyutilib.autotest')
 
 from pyutilib.autotest.plugins import ITestDriver, TestDriverFactory, ITestParser, TestDriverBase
 from pyutilib.autotest.driver import run, main, create_test_suites
-import pyutilib.autotest.yaml_plugin
-import pyutilib.autotest.json_plugin
-import pyutilib.autotest.default_testdriver
 
 pyutilib.component.core.PluginGlobals.pop_env()
